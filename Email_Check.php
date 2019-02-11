7ca733caeeae0cb476e2a901911f85c6d0433f15
Marjay <marjay_gabriel_tapay@dlsl.edu.ph>
2019-02-12 06:02:38 +0800

    testing CORS

--- Email_Check.php
+++ Email_Check.php
@@ -2,7 +2,11 @@
 
 require 'ConnectDB.php';
 header('Content-Type: application/json');
-
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Max-Age: 1000");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
+header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
 $username = "";
 $password = "";
 function verify($conn,$email)
