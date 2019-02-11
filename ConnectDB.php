013530c2e070110aad4e3372c63af46a81ae9d3c
Marjay <marjay_gabriel_tapay@dlsl.edu.ph>
2019-02-12 05:24:06 +0800

    Updated ConnectDb

--- ConnectDB.php
+++ ConnectDB.php
@@ -1,12 +1,7 @@
 <?php
-$db_name = "heroku_41516de6f172a28";
-$mysql_user = "bc88d4488c8d2a";
-$mysql_pass = "8183538a";
+$db_name = "heroku_965df3cd76177bf";
+$mysql_user = "bffc36c60eb244";
+$mysql_pass = "0f4f564c";
 $servername = "us-cdbr-iron-east-03.cleardb.net";
 $conn = mysqli_connect($servername,$mysql_user,$mysql_pass,$db_name);
-
-
  ?>
-
-
-https://requench-fireapp.firebaseapp.com/__/auth/handler?apiKey=AIzaSyDyQqFON2gburXUvS4bfPTgX7oPIn7gg0g&appName=%5BDEFAULT%5D&authType=signInViaRedirect&providerId=google.com&scopes=profile&redirectUrl=https%3A%2F%2Frequench.herokuapp.com%2F&v=5.8.0
