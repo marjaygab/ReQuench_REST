b53f632a7ab198a4080b541dd2f86dda2cdbc374
Marjay Tapay <tapaymarjay@gmail.com>
2019-02-13 18:58:06 +0800

    Added Cashier. Not Yet tested

---
+++ Customer_Login.php
@@ -0,0 +1,99 @@
+<?php
+    require 'ConnectDB.php';
+
+
+    function checkAcc($conn,$RFID_ID)
+    {
+        $query = "SELECT * FROM accounts WHERE RFID_ID = '$RFID_ID'";
+        $result = mysqli_query($conn,$query);
+        $response = array();
+        if (mysqli_num_rows($result) == 1) {
+            $response['Success'] = true;
+            $row = mysqli_fetch_assoc($result);
+            $response['Acc_ID'] = $row['Acc_ID'];
+        } else if (mysqli_num_rows($result) > 1) {
+            //error occured. Duplicate account detected
+            $response['Success'] = false;
+        }else{
+            $response['Success'] = false;
+        }
+        return $response;
+    }
+
+    function checkUnrec($conn,$RFID_ID)
+    {
+        $query = "SELECT * FROM unrecorded_users WHERE RFID_ID = '$RFID_ID'";
+        $result = mysqli_query($conn,$query);
+        $response = array();
+        if (mysqli_num_rows($result) == 1) {
+            $response['Success'] = true;
+            $row = mysqli_fetch_assoc($result);
+            $response['UU_ID'] = $row['UU_ID'];
+        } else if (mysqli_num_rows($result) > 1) {
+            //error occured. Duplicate account detected
+            $response['Success'] = false;
+        }else{
+            $response['Success'] = false;
+        }
+        return $response;
+    }
+    function getAcc($conn,$Acc_ID)
+    {
+        $query = "SELECT * FROM accounts WHERE Acc_ID = '$Acc_ID'";
+        $result = mysqli_query($conn,$query);
+        $row = array();
+        if (mysqli_num_rows($result) == 1) {
+            while($r = mysqli_fetch_assoc($result)){
+                $row[] = $r;
+            }
+            return $row;
+        } else if (mysqli_num_rows($result) > 1) {
+            //error occured. Duplicate account detected
+            return false;
+        }else{
+            return false;
+        }
+    }
+
+
+
+    function createUnrec($conn,$RFID_ID)
+    {
+        $Balance = 0;
+        $query = "INSERT INTO unrecorded_users (RFID_ID,Balance) VALUES ('$RFID_ID','$Balance')";
+        if (mysqli_query($conn,$query)) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    
+    $contents = file_get_contents('php://input');
+    $response = array();
+    if ($contents != NULL) {
+        $data = json_decode($contents);
+
+        $RFID_ID = $data -> {"RFID_ID"};
+
+        $rfid_present = checkAcc($conn,$RFID_ID);
+        if ($rfid_present['Success']) {
+            $response['Success'] = true;
+            $response['Account'] = getAcc($conn,$rfid_present['Acc_ID']);
+        } else {
+            //create unrecorded account
+            if (createUnrec($conn,$RFID_ID)) {
+                $response['Success'] = true;
+            } else {
+                $response['Success'] = false;
+            }
+        }
+        
+        
+    } else {
+        $response['Success'] = false;    
+    }
+    
+    echo json_encode($response);
+    mysqli_close($conn);
+?>
