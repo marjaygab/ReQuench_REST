5d41936a68980475e83e62bf2bbcd1f235af8cce
Marjay <marjay_gabriel_tapay@dlsl.edu.ph>
2019-02-16 12:59:45 +0800

    Machine Init now fetched unrecorded

--- Machine_Initialize.php
+++ Machine_Initialize.php
@@ -4,6 +4,9 @@ ini_set('display_errors', 'On');
 error_reporting(E_ALL); 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
+
+
+
 function fetchRecentPurchase($conn,$Acc_ID)
 {
   $query = "SELECT * FROM purchase_history WHERE Acc_ID = '$Acc_ID'";
@@ -36,9 +39,65 @@ function fetchRecentTransaction($conn,$Acc_ID)
 }
 
 
-function getAccountsDetails($conn,$response,$otp_entered)
+function fetchRecentPurchaseUnrecorded($conn,$UU_ID)
 {
-    $query = "SELECT accounts.Acc_ID,acc_levels.Access_Level FROM accounts INNER JOIN acc_levels ON accounts.AL_ID = acc_levels.AL_ID WHERE OTP = '$otp_entered'";
+  $query = "SELECT * FROM purchase_history_unrec WHERE UU_ID = '$UU_ID'";
+  $result = mysqli_query($conn,$query);
+  $row = array();
+  if (mysqli_num_rows($result) > 0) {
+    while($r = mysqli_fetch_assoc($result)){
+      $row[] = $r;
+    }
+    return $row;
+  }else{
+    return null;
+  }
+}
+
+function fetchRecentTransactionUnrecorded($conn,$UU_ID)
+{
+  // code...
+  $query = "SELECT * FROM transaction_history_unrec WHERE UU_ID = '$UU_ID'";
+  $result = mysqli_query($conn,$query);
+  $row = array();
+  if (mysqli_num_rows($result) > 0) {
+    while($r = mysqli_fetch_assoc($result)){
+      $row[] = $r;
+    }
+    return $row;
+  }else{
+    return null;
+  }
+}
+
+
+
+function getUnrecordedAccountsDetails($conn,$response,$rfid_id = NULL)
+{
+    $query = "SELECT * FROM unrecorded_users WHERE RFID_ID = '$rfid_id'";
+    $result = mysqli_query($conn,$query);
+    if (mysqli_num_rows($result) > 0) {
+            $row = mysqli_fetch_assoc($result);
+            $response['Account'] = $row;
+            $response['Success'] = true;
+            return $response;
+    }else {
+      return NULL;
+    }
+}
+
+function getAccountsDetails($conn,$response,$otp_entered = NULL,$rfid_id = NULL,$login_method)
+{
+
+    if ($login_method == 'OTP' && $otp_entered != NULL) {
+      $condition = "WHERE OTP = '$otp_entered'";
+    } else if($login_method == 'RFID' && $rfid_id != NULL){
+      $condition = "WHERE RFID_ID = '$rfid_id'";
+    }else{
+      return false;
+    }
+
+    $query = "SELECT accounts.Acc_ID,acc_levels.Access_Level FROM accounts INNER JOIN acc_levels ON accounts.AL_ID = acc_levels.AL_ID $condition";
     $result = mysqli_query($conn,$query);
     if (mysqli_num_rows($result) > 0) {
     $Acc_ID = '';
@@ -94,23 +153,47 @@ function getAccountsDetails($conn,$response,$otp_entered)
     }
 }
 
+
 $contents = file_get_contents('php://input');
 if ($contents != null) {
+    $rfid_id = NULL;
+    $otp_entered = NULL;
     $data = json_decode($contents);
-    $otp_entered = $data->{"OTP_Entered"};
+    $login_method = $data -> {"Login_Method"};
+    if ($login_method == 'OTP') {
+      $otp_entered = $data->{"OTP_Entered"};
+    } else if($login_method == 'RFID'){
+      $rfid_id = $data->{"RFID_ID"};
+    }
+  
     $response = array();
-    $response = getAccountsDetails($conn,$response,$otp_entered);
+    $response_unrecorded = array();
+    $response = getAccountsDetails($conn,$response,$otp_entered,$rfid_id,$login_method);
+    $response_unrecorded =  getUnrecordedAccountsDetails($conn,$response,$rfid_id);
     $returned_accid = $response['Account']['Acc_ID'];
+    $returned_uuid = $response_unrecorded['Account']['UU_ID'];
     if ($returned_accid != NULL) {
         $response['Success'] = true;
         $purchase_history = fetchRecentPurchase($conn,$returned_accid);
         $transaction_history = fetchRecentTransaction($conn,$returned_accid);
         $response['Purchase_History'] = $purchase_history;
         $response['Transaction_History'] = $transaction_history;
-    }else{
-        $response['Success'] = false;
+        $response['Account_Type'] = 'Recorded';
+        echo json_encode($response);
+    }else if ($returned_uuid != NULL) {
+      $response['Success'] = true;
+      $purchase_history = fetchRecentPurchaseUnrecorded($conn,$returned_uuid);
+      $transaction_history = fetchRecentTransactionUnrecorded($conn,$returned_uuid);
+      $response_unrecorded['Purchase_History'] = $purchase_history;
+      $response_unrecorded['Transaction_History'] = $transaction_history;
+      $response_unrecorded['Account_Type'] = 'Unrecorded';
+      echo json_encode($response_unrecorded);
     }
-    echo json_encode($response);
+    else{
+        $response['Success'] = false;
+        echo json_encode($response);
+    }
+    
 }
 mysqli_close($conn);
 
