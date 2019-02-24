078eff636d23f69857da9d2ffa8e70b285950187
Marjay Tapay <tapaymarjay@gmail.com>
2019-02-24 15:10:41 +0800

    Testing

--- Fetch_All_Transaction.php
+++ Fetch_All_Transaction.php
@@ -7,6 +7,7 @@ header("Access-Control-Max-Age: 1000");
 header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
 header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
 $account_id = $_POST["Acc_ID"];
+$response = array();
 $query = "SELECT transaction_history.Acc_ID,machine_unit.Machine_Location,transaction_history.Date,transaction_history.Time,transaction_history.Amount,transaction_history.Temperature
 ,transaction_history.Price_Computed
 FROM transaction_history
@@ -19,6 +20,7 @@ if (mysqli_num_rows($result) > 0) {
   while ($r = mysqli_fetch_assoc($result)) {
     $rows[] = $r;
   }
+  $response['Transaction_List'] = $row;
   echo json_encode($rows,JSON_PRETTY_PRINT);
 }
 mysqli_close($conn);
