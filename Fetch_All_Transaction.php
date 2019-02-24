83c07cb6827fe076fbfc47f18b054632c2b0945a
Marjay Tapay <tapaymarjay@gmail.com>
2019-02-24 18:15:10 +0800

    Testing

--- Fetch_All_Transaction.php
+++ Fetch_All_Transaction.php
@@ -12,17 +12,36 @@ $query = "SELECT transaction_history.Acc_ID,machine_unit.Machine_Location,transa
 ,transaction_history.Price_Computed
 FROM transaction_history
 INNER JOIN machine_unit ON transaction_history.MU_ID = machine_unit.MU_ID";
+
+$query2 = "SELECT transaction_history_unrec.UU_ID,machine_unit.Machine_Location,transaction_history_unrec.Date,transaction_history_unrec.Time,transaction_history_unrec.Amount,transaction_history_unrec.Temperature
+,transaction_history_unrec.Price_Computed
+FROM transaction_history_unrec
+INNER JOIN machine_unit ON transaction_history_unrec.MU_ID = machine_unit.MU_ID";
+
 $result = mysqli_query($conn,$query);
+$result2 = mysqli_query($conn,$query2);
 
 if (mysqli_num_rows($result) > 0) {
-  $rows = array();
-
-  while ($r = mysqli_fetch_assoc($result)) {
-    $rows[] = $r;
-  }
-  $response['Transaction_List'] = $rows;
-  echo json_encode($response,JSON_PRETTY_PRINT);
+    $rows = array();
+    while ($r = mysqli_fetch_assoc($result)) {
+        $rows[] = $r;
+    }
+    $response['Transaction_List'] = $rows;
+}else{
+    $response['Transaction_List'] = '';
 }
+
+if (mysqli_num_rows($result2) > 0) {
+    $rows = array();
+    while ($r = mysqli_fetch_assoc($result2)) {
+        $rows[] = $r;
+    }
+    $response['Transaction_List_Unrecorded'] = $rows;
+}else{
+    $response['Transaction_List_Unrecorded'] = '';
+}
+
+echo json_encode($response,JSON_PRETTY_PRINT);
 mysqli_close($conn);
 
 ?>
