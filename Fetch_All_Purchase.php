7fbf089f144321e944f30024212b94159d3d0fd4
Marjay <marjay_gabriel_tapay@dlsl.edu.ph>
2019-03-17 09:49:25 +0800

    Addde Fetch All Purchases

---
+++ Fetch_All_Purchase.php
@@ -0,0 +1,42 @@
+<?php 
+require 'ConnectDB.php';
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Max-Age: 1000");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
+header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
+$response = array();
+$query = "SELECT purchase_history.Acc_ID,purchase_history.Date,purchase_history.Time,purchase_history.Amount,purchase_history.Price_Computed
+FROM purchase_history";
+
+$query2 = "SELECT purchase_history_unrec.Acc_ID,purchase_history_unrec.Date,purchase_history_unrec.Time,purchase_history_unrec.Amount,purchase_history_unrec.Price_Computed
+FROM purchase_history_unrec";
+
+$result = mysqli_query($conn,$query);
+$result2 = mysqli_query($conn,$query2);
+
+if (mysqli_num_rows($result) > 0) {
+    $rows = array();
+    while ($r = mysqli_fetch_assoc($result)) {
+        $rows[] = $r;
+    }
+    $response['Purchase_List'] = $rows;
+}else{
+    $response['Purchase_List'] = '';
+}
+
+if (mysqli_num_rows($result2) > 0) {
+    $rows = array();
+    while ($r = mysqli_fetch_assoc($result2)) {
+        $rows[] = $r;
+    }
+    $response['Purchase_List_Unrecorded'] = $rows;
+}else{
+    $response['Purchase_List_Unrecorded'] = '';
+}
+
+echo json_encode($response,JSON_PRETTY_PRINT);
+mysqli_close($conn);
+
+?>
