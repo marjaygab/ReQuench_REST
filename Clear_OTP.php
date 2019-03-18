f76805ea4379ed5a0b84193ff619e47d1ffd7c66
Marjay <marjay_gabriel_tapay@dlsl.edu.ph>
2019-03-19 02:50:39 +0800

    Testing

--- Clear_OTP.php
+++ Clear_OTP.php
@@ -15,46 +15,17 @@ require 'ConnectDB.php';
 
 
 if(isset($_POST['Acc_ID'])){
-
-
   $Acc_ID = $_POST['Acc_ID'];
-
-
   $response = array("Updated"=>false);
-
-
     $query = "UPDATE accounts SET OTP = null WHERE Acc_ID='$Acc_ID'";
-
-
     if (mysqli_query($conn,$query)) {
-
-
         $response['Updated'] = true;
-
-
     }else{
-
-
         $response['Updated'] = false;
-
-
     }
-
-
   echo json_encode($response,JSON_PRETTY_PRINT);
-
-
 }
-
-
 mysqli_close($conn);
-
-
-
-
-
-
-
 
  ?>
 
--- Fetch_Image.php
+++ Fetch_Image.php
@@ -14,81 +14,39 @@ header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
 
 
 
-if (isset($_POST["Acc_ID"])) {
-
-
-  $acc_id = $_POST["Acc_ID"] ;
-
-
-
-
-
+function fetchImage($conn,$acc_id){
   $query = "SELECT Image_Path FROM acc_images WHERE Acc_ID = '$acc_id'";
-
-
   $result = mysqli_query($conn,$query);
-
-
-
-
-
   if (mysqli_num_rows($result) == 1) {
-
-
     // $output = mysqli_fetch_assoc();
-
-
-
-
-
     while ($row = mysqli_fetch_assoc($result)) {
-
-
       $file_path = $row["Image_Path"];
-
-
     }
-
-
+    $file_path = "https://requench-rest.herokuapp.com" . $file_path;
     $response = array();
-
-
     $response["image"] = base64_encode(file_get_contents($file_path));
-
-
     $response["Success"] = "true";
-
-
   }else{
-
-
     //No Image Exists for specific User. Display Default Image
-
-
     $response["Success"] = "false";
-
-
   }
-
-
-
-
-
-  echo json_encode($response,JSON_PRETTY_PRINT);
-
-
-
-
-
+    return $response;
 }
 
 
 
+$contents = file_get_contents('php://input');
+$response = array();
+if ($contents != null) {
+    $data = json_decode($contents);
+    $Acc_ID = $data->{"Acc_ID"};
+    $response = fetchImage($conn,$Acc_ID);
+} else {
+    $response['Success'] = false;
+    echo json_encode($response);
+}
 
-
-
-
-
+echo json_encode($response,JSON_PRETTY_PRINT);
 mysqli_close($conn);
 
 
