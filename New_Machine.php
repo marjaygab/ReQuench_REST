3463b607c270fc85a5aee18034eb4900e136e2da
Marjay Tapay <tapaymarjay@gmail.com>
2019-04-04 17:50:04 +0800

    Added Remove Machine

--- New_Machine.php
+++ New_Machine.php
@@ -1,7 +1,7 @@
 <?php
-
-
 require 'ConnectDB.php';
+require 'API_Key_Checker.php';
+require 'Generate_API_Key.php';
  header('Content-Type: application/json');
  header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Credentials: true");
@@ -15,16 +15,53 @@ function addMachine($conn,$secret_key)
   $response = array();
   $find_result = findSecretKey($conn,$secret_key);
     if($find_result != false){
-        $result = getMachine($conn,$find_result);
-        if ($result != null) {
-            $response['Success'] = true;
-            $response['Machine'] = $result;
+        $API_KEY = renewAPIKey($conn,$secret_key);
+        if ($API_KEY != NULL) {
+            $result = getMachine($conn,$find_result);
+            if ($result != null) {
+                $response['Success'] = true;
+                $response['Secret_Key'] = $secret_key;
+                $response['Machine'] = $result;   
+            }else{
+                $response['Success'] = false;
+                $response['Machine'] = null;
+                $response['Secret_Key'] = $secret_key;
+            }    
+        } else {
+            $response['Success'] = false;
         }
     }else{
         $response['Success'] = false;
     }
   echo json_encode($response,JSON_PRETTY_PRINT);
 }
+
+
+function renewAPIKey($conn,$Secret_Key)
+{
+    $response = array();
+    $query = "SELECT MU_ID FROM secret_list WHERE Secret_Key = '$Secret_Key'";
+    $result = mysqli_query($conn,$query);
+    if (mysqli_num_rows($result) == 1) {
+        $row = mysqli_fetch_assoc($result);
+        $MU_ID = $row['MU_ID'];
+
+        $API_KEY = generateKey();
+        while(true){
+            $API_KEY = generateKey();
+            if (!checkApi($conn,$API_KEY)['Success']) {
+                $query = "UPDATE machine_unit SET API_KEY = '$API_KEY' WHERE MU_ID = $MU_ID";
+                if(mysqli_query($conn,$query)){
+                    break;
+                };
+            }
+        }
+    }else{
+        $API_KEY = NULL;
+    }
+    return $API_KEY;
+}
+
 
 function getMachine($conn,$MU_ID)
 {
