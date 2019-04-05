6c0bc20195b3b30068ca6ca182fb52d42b660aad
Marjay Tapay <tapaymarjay@gmail.com>
2019-04-05 08:48:44 +0800

    Testing

---
+++ Fetch_Machine.php
@@ -0,0 +1,43 @@
+<?php 
+require 'ConnectDB.php';
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Max-Age: 1000");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
+header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
+$response = array();
+
+
+function getMachine($conn,$MU_ID)
+{
+    $query = "SELECT * FROM machine_unit WHERE MU_ID = '$MU_ID'";
+    $result = mysqli_query($conn,$query);
+    if (mysqli_num_rows($result) == 1) {
+        $rows = array();
+        while ($r = mysqli_fetch_assoc($result)) {
+            $rows[] = $r;
+        }
+        $response['Machine'] = $rows;
+        $response['Success'] = true;
+    }else{
+        $response['Machines'] = '';
+        $response['Success'] = false;
+    }
+    echo json_encode($response,JSON_PRETTY_PRINT);
+}
+
+
+$contents = file_get_contents('php://input');
+if ($contents != null) {
+  $data = json_decode($contents);
+  $Acc_ID = $data->{"MU_ID"};
+  getMachine($conn,$MU_ID);
+}else{
+  die("An Error Occured");
+}
+
+
+mysqli_close($conn);
+
+?>
